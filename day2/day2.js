bcb4fd8a5df25347c08330870d7390f6fef9d7f8
LucieCicvarkova <46781332+LucieCicvarkova@users.noreply.github.com>
2021-12-02 21:31:49 +0100

    day2 completed

--- day2/day2.js
+++ day2/day2.js
@@ -2,22 +2,43 @@ const fs = require("fs");
 const input = fs.readFileSync("./day2input.txt", "utf8");
 const inputArray = input.split("\n").map((item) => item.replace(/\r/, ""));
 
+/* ---Part 1--- */
 let horizontalCount = 0;
 let verticalCount = 0;
-const forward = new RegExp("forward*");
-const up = new RegExp("up*");
-const down = new RegExp("down*");
 
-for (i = 0; i < inputArray.length; i++) {
-  if (forward.test(inputArray[i])) {
-    horizontalCount = horizontalCount + parseInt(inputArray[i].charAt(inputArray[i].length - 1));
+for(let i = 0; i< inputArray.length; i++) {
+  let inputItem = inputArray[i].split(' ');
+  if(inputItem[0]==='forward') {
+    horizontalCount = horizontalCount + parseInt(inputItem[1]);
   }
-  if (up.test(inputArray[i])) {
-    verticalCount = verticalCount - parseInt(inputArray[i].charAt(inputArray[i].length - 1));
-  }
-  if (down.test(inputArray[i])) {
-    verticalCount = verticalCount + parseInt(inputArray[i].charAt(inputArray[i].length - 1));
-  }
+  if(inputItem[0]==='up') {
+    verticalCount = verticalCount - parseInt(inputItem[1]);
+  } 
+  if(inputItem[0]==='down') {
+    verticalCount = verticalCount + parseInt(inputItem[1]);
+  } 
 }
 
 console.log(verticalCount*horizontalCount);
+
+/*--- Part 2 ---*/
+
+let aim = 0;
+let horizontalPos= 0;
+let depth = 0;
+
+for(let i = 0; i< inputArray.length; i++) {
+  let inputItem = inputArray[i].split(' '); 
+  if(inputItem[0]==='down') {
+    aim = aim + parseInt(inputItem[1]);
+  }  
+  if(inputItem[0]==='up') {
+    aim = aim - parseInt(inputItem[1]);
+  }  
+  if(inputItem[0]==='forward') {
+    horizontalPos = horizontalPos + parseInt(inputItem[1]);
+    depth = depth + aim*parseInt(inputItem[1]);
+  }
+}
+
+console.log(horizontalPos*depth);
